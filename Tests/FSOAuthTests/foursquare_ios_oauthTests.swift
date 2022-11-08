bc2df08c38a90e815e1a01c3533a4f9039713455
Mark Townsend <markltownsend@users.noreply.github.com>
2022-11-07 19:46:30 -0800

    convert to package

---
+++ Tests/FSOAuthTests/foursquare_ios_oauthTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import FSOAuth
+
+final class foursquare_ios_oauthTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+}
